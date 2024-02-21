33619f3ccff285d2ee593631c0380c762c8f711e
Kaitlyn-Greenwood <159150107+Kaitlyn-Greenwood@users.noreply.github.com>
2024-02-21 13:53:57 +0000

    added name and email

--- handler.php
+++ handler.php
@@ -8,6 +8,11 @@
   </head>
 
   <body>
+  <form action="welcome.php" method="post">
+  Name: <input type="text" name="name"><br>
+  E-mail: <input type="text" name="email"><br>
+  <input type="submit">
+</form>
     
     <?php
     $userName = "";
